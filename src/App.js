76a08c7b492c1d3dabde9950a9f6b146badbe71e
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-09 11:50:18 -0700

    Import PostPage and SearchBar components and add them to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-/* 
-Start here and then work through the 
-PostsContainer components and the CommentSection Components. 
-Not all files need code added. 
-Look at each file to see where you need to pass props or add code 
+/*
+Start here and then work through the
+PostsContainer components and the CommentSection Components.
+Not all files need code added.
+Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-
+import PostsPage from "./components/PostsContainer/PostsPage.js";
+import SearchBarContainer from "./components/SearchBar/SearchBarContainer.js";
 
 const App = () => {
-
   return (
     <div className="App">
       {/* Add imported components here to render them */}
+      <SearchBarContainer />
+      <PostsPage />
     </div>
   );
 };
