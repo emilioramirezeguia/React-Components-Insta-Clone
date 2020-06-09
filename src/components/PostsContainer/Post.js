4b2fb6f90782f0143f9371384b648f549a80eabd
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-09 13:41:35 -0700

    Update post likes dynamically

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -7,16 +7,17 @@ import PostHeader from "./PostHeader";
 import "./Posts.css";
 
 // pass props in this file to
-const Post = props => {
+const Post = (props) => {
   // set up state for the likes
-
+  const [likes, setLikes] = useState(0);
+  const updateLikes = function (event) {
+    return setLikes(likes + 1);
+  };
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
@@ -25,7 +26,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection likes={likes} updateLikes={updateLikes} />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
